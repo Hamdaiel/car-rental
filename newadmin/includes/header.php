b011597506b9f3b4bf88c121561544a66ba6a209
Hamdaiel <hamdaiel@gmail.com>
2019-11-04 12:22:24 +0300

    javascritp matterials for the admin page

---
+++ newadmin/includes/header.php
@@ -0,0 +1,13 @@
+<div class="brand clearfix" style="background-color: #232323;">
+	<a href="dashboard.php" style="position:absolute; left: 250px; padding-top:10px; font-size: 20px; ">CRMS | Admin Panel</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav" >
+			
+			<li class="ts-account" style="">
+				<a href="#" style="background-color: #232323;"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ newadmin/includes/leftbar.php
@@ -0,0 +1,18 @@
+	<nav class="ts-sidebar" style="background-color: #232323;">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"> Home</a></li>
+
+<li><a href="#">Vehicles</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"> Manage Reservations</a></li>
+				<li><a href="feedback.php"> Feedback</a></li>
+				<li><a href="addPayment.php"> Add Payment</a></li>
+
+			</ul>
+		</nav>
